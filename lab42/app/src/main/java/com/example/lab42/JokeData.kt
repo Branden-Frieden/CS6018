c2934818255e7246b182df7e0527e314f935c88b
Branden-Frieden <bcfrieden@gmail.com>
2023-09-26 16:25:41 -0600

    added lab4(2) listed as lab42 on accident

---
+++ lab42/app/src/main/java/com/example/lab42/JokeData.kt
@@ -0,0 +1,10 @@
+package com.example.lab42
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//Defines a SQLITE table, basically
+@Entity(tableName="jokes")
+data class JokeData(var joke: String){
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0 // integer primary key for the DB
+}
